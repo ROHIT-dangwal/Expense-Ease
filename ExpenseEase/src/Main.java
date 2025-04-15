c09006af3a475ca037541bb97ab2bf04580ede0d
ROHIT-dangwal <rdsd21104@gmail.com>
2025-04-15 10:41:52 +0530

    changes done

---
+++ ExpenseEase/src/Main.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.net.URL;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL location = getClass().getResource("home.fxml");
+        Parent root = FXMLLoader.load(location);
+        primaryStage.setTitle("ExpenseEase");
+
+        Scene scene = new Scene(root, 1200, 700);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
---
+++ ExpenseEase/src/MainController.java
@@ -0,0 +1,26 @@
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainController {
+
+    @FXML
+    private void initialize() {
+        System.out.println("Controller initialized");
+    }
+
+    @FXML
+    private void handleAddExpense(ActionEvent event) {
+        System.out.println("Add Expense button clicked");
+    }
+
+    @FXML
+    private void handleViewExpenses(ActionEvent event) {
+        System.out.println("View Expenses button clicked");
+    }
+
+    @FXML
+    private void handleExit(ActionEvent event) {
+        Platform.exit();
+    }
+}
